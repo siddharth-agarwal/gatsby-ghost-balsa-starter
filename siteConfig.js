d35953ad31665ddaf0b2e3ab18ec2630ed1aa13d
siddharth agarwal <siddharth.a@outlook.com>
2020-05-15 15:28:21 -0400

    update README + site-specific vars in siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    siteUrl: `https://ghost-balsa-preview.draftbox.co/`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://blog.thedeep3nd.io`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Built with Draftbox`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Lightning fast, secure front-end for your WordPress or Ghost blog, without coding.`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteDescriptionMeta: `some thoughts on topics that interest me`,
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Built with Draftbox`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    shortTitle: `thedeep3nd.io`,
+    siteIcon: `me.png`,
+    backgroundColor: `#e9e9e9`,
+    themeColor: `#15171A`,
 }
